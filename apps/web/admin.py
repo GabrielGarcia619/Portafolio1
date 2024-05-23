72ced7fd83ed43e82f450c4057ed760152af2544
GabrielGarcia619 <gabrielgarciahuayhua619@gmail.com>
2024-05-23 19:18:32 -0400

    Agregacion de una base de Datos

--- apps/web/admin.py
+++ apps/web/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import PerfilProfecional
 
 # Register your models here.
+admin.site.register(PerfilProfecional)
--- apps/web/views.py
+++ apps/web/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import PerfilProfecional
 
-# Create your views here.
-class index(TemplateView):
-    template_name = 'index.html'  # El nombre del archivo html a cargar
-        # context [''] = 'Esta es una plantilla de ejemplo de django '
+class index(TemplateView):  # Se recomienda que las clases de vista tengan nombres en CamelCase
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['perfil'] = PerfilProfecional.objects.first()  # Asegúrate de que este sea el modelo correcto
+        return context
